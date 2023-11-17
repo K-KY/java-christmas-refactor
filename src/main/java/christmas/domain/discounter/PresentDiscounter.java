9dc3f27d57ddd98ebc9bdf67e749165b0d8d550c
K-KY <kyuyoungk@naver.com>
2023-11-18 07:24:18 +0900

    Refactor(PresentDiscounter) : Benefit객체에 증정 이벤트 내역을 추가하고 이벤트 금액을 러턴

--- src/main/java/christmas/domain/discounter/PresentDiscounter.java
+++ src/main/java/christmas/domain/discounter/PresentDiscounter.java
@@ -1,22 +1,25 @@
 package christmas.domain.discounter;
 
+import christmas.domain.orderinfo.Benefit;
 import christmas.domain.orderinfo.OrderedAmount;
+import java.text.DecimalFormat;
 
 public class PresentDiscounter {
-    private static final String PRESENT = "샴페인 1개";
-    private static final String DEFAULT = "";
+    private static final String PRICE_FORMAT = "-###,###원";
+    private static final DecimalFormat MONEY_FORMAT = new DecimalFormat(PRICE_FORMAT);
     private static final int DISCOUNT = 25000;
+    private static final int NO_DISCOUNT = 0;
     private final OrderedAmount orderedAmount;
 
     public PresentDiscounter(OrderedAmount orderedAmount) {
         this.orderedAmount = orderedAmount;
     }
 
-    public String discount() {
+    public int discount(Benefit benefit) {
         if (orderedAmount.present()) {
-            orderedAmount.discount(DISCOUNT);
-            return PRESENT;
+            benefit.addBenefit("증정 이벤트: " + MONEY_FORMAT.format(DISCOUNT));
+            return DISCOUNT;
         }
-        return DEFAULT;
+        return NO_DISCOUNT;
     }
 }
