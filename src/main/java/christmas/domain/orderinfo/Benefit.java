335b467d38b136aa64fc485b725638ea2bc0c856
K-KY <kyuyoungk@naver.com>
2023-11-18 07:01:51 +0900

    Feat(Benefit) : 증정 이벤트 내역이 있으면 "샴페인 1개" 를 리턴

--- src/main/java/christmas/domain/orderinfo/Benefit.java
+++ src/main/java/christmas/domain/orderinfo/Benefit.java
@@ -5,6 +5,9 @@ import java.util.List;
 
 public class Benefit {
     private static final String NEW_LINE = "\n";
+    private static final String PRESENT_HISTORY = "증정 이벤트: -25,000원";
+    private static final String NO_PRESENT = "없음";
+    private static final String PRESENT = "샴페인 1개";
     private final List<String> ownBenefit = new ArrayList<>();
 
     public void addBenefit(String benefit) {
@@ -16,5 +19,12 @@ public class Benefit {
         StringBuilder stringBuilder = new StringBuilder();
         ownBenefit.forEach(benefits -> stringBuilder.append(benefits).append(NEW_LINE));
         return stringBuilder.toString();
+    }
+
+    public String present() {
+        if (ownBenefit.contains(PRESENT_HISTORY)) {
+            return PRESENT;
+        }
+        return NO_PRESENT;
     }
 }
